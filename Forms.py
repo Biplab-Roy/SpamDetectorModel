b18e7d94fd9916755dc987fc308fa745a847d548
Biplab-Roy <47073892+Biplab-Roy@users.noreply.github.com>
2020-05-29 12:23:20 +0530

    Delete Forms.py

--- Forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Length, DataRequired, Email
-
-class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(),Length(max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    message = StringField('Message', validators=[DataRequired()])
-    submit = SubmitField('Submit')
